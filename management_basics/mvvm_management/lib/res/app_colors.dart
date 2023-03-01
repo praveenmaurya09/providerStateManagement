5170431f2992d6c6ca607ad39e15ac0d0284d4f1
Praveen Maurya <112618229+TheUltimate-Maurya@users.noreply.github.com>
2023-03-01 20:51:04 +0530

    Some changes
    Completed

--- management_basics/mvvm_management/lib/res/app_colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-
-class AppColors{
-  static const Color blackColor = Color(0xFF000000);
-  static const Color whiteColor = Color(0xFFFFFFFF);
-  static const Color buttonColor = Colors.blueAccent;
-}
